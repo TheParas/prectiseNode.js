86ef8c9ce99d5b2c096a5a57c5d6895e61c31eac
TheParas <parasramawat07@gmail.com>
2025-07-12 15:19:50 +0530

    practising func & callback func

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // by using tab it will automatically type in node
-console.log ('server file is running')
+//console.log ('server file is running')
 //we use node in terminal to run js.
+//after installing nodemon we will write nodemon server.js to start it to detect & run on every change.
+// function add(a,b) {
+//   return a+b;
+// }
+
+// var add = (a,b) =>  a + (2*b)
+//  console.log(add(4,50));
+
+ 
+// const add = function (a,b,c) {
+//    c();
+//    'a + b'=== 'sum';
+//  console.log( 'a + b ') ;
+
+// }
+// add(2,3,() => console.log('add completed'));
