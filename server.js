a021be2aabeab3115157eded72f7cf76282fc2c9
TheParas <parasramawat07@gmail.com>
2025-07-12 15:46:46 +0530

    installing loadash in my file

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-// by using tab it will automatically type in node
-//console.log ('server file is running')
-//we use node in terminal to run js.
-//after installing nodemon we will write nodemon server.js to start it to detect & run on every change.
-// function add(a,b) {
-//   return a+b;
-// }
+var fs = require('fs');
+var os = require('os');
 
-// var add = (a,b) =>  a + (2*b)
-//  console.log(add(4,50));
-
- 
-// const add = function (a,b,c) {
-//    c();
-//    'a + b'=== 'sum';
-//  console.log( 'a + b ') ;
-
-// }
-// add(2,3,() => console.log('add completed'));
+var user = os.userInfo('os');
+console.log(user.username);
