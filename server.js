6f592a35562935b86992165ec5b8cd859b575179
TheParas <parasramawat07@gmail.com>
2025-07-17 16:53:27 +0530

    replace data to response . imp.

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/person',async (req, res) =>
           const menuItem = new MenuItem(data); 
           const response = await menuItem.save(); //save the new menu item to the database
           console.log('Menu item saved');
-          res.status(200).json(data); //send the data as response back to the client
+          res.status(200).json(response); //send the data as response back to the client
       } catch (error) {
           console.error('Error:', error);
           res.status(500).json({ error: 'Internal Server Error' }); //send error response
@@ -71,7 +71,8 @@ app.post('/person',async (req, res) =>
       try {
         const data = await MenuItem.find(); //find() method se sabhi menu items ka data mil jayega
         console.log('Menu items fetched');
-        res.status(200).json(data); //send the response back to the client
+        res.status(200).json(response); //send the response back to the client
+        // response -> actualnObject, //data -> raw input, response is good because it checks.
       }catch {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal Server Error' }); //send error response
