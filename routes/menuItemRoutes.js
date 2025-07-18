da4d3ddea3a5b47d21860611f9f3618316d5c961
TheParas <parasramawat07@gmail.com>
2025-07-18 17:45:29 +0530

    completed day 6 excercises

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -29,5 +29,57 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:taste' , async (req, res) => {
+  try{
+const tasteFeel = req.params.taste;//extracting the taste id from the URL parameter
+if(tasteFeel === 'Sour' || tasteFeel === 'Sweet' || tasteFeel === 'Spicy') {
+//we have defined only 3 types of taste in a dish.
+const menuItem  = await MenuItem.find({ taste: tasteFeel})
+// sending the list of items of given taste  as a JSON response
+res.json(menuItem);
+} else {
+  res.status(404).json({ error: 'Invalid taste type' });
+}
+ }catch(error) {
+    console.error('Error fetching persons:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
+router.put('/:id' , async (req, res) => { 
+  try{
+        //:id is a variable name , it holds the value of unique id.
+        const itemId = req.params.id; // extract the item id, which is automatically saved in body parser.
+        const updatedItem = req.body; //saving what we need to to update from req.body to a variable.
+        const response = await MenuItem.findByIdAndUpdate(itemId, updatedItem, {
+          new: true, //Returning the new updated document as a response
+          runValidators: true //run validations on the updated data
+        })
+        if(!response) {
+          return res.status(404).json({error: 'item not found'});
+        }
+          console.log('item updated');
+          res.status(200).json(response);
+  }catch 
+(error){
+      console.error('Error fetching persons:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
+router.delete('/:id', async(req, res) => { 
+  try{
+ const itemId = req.params.id;
+ const response = await MenuItem.findByIdAndDelete(itemId)
+  if(!response) {
+          return res.status(404).json({error: 'item not found'});
+        }
+            console.log('item updated');
+          res.status(200).json(response);
+  } catch(error){
+      console.error('Error fetching persons:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+  }
+)
 module.exports = router;
